683ed2f66068e2d7bc9cc05e14078431cea8a07d
eric <ericrad@bk.ru>
2024-04-02 14:18:55 +0300

    первоначальные модели и миграции

--- pereval/camp/models.py
+++ pereval/camp/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class Users(models.Model):
+class Tourist(models.Model):
     email = models.EmailField(max_length=128, unique=True)
     last_name = models.CharField(max_length=128)
     first_name = models.CharField(max_length=128)
@@ -24,14 +24,19 @@ class Pereval(models.Model):
     a_2 = '2A'
     a_3 = '3A'
     a_4 = '4A'
-    LEVEL_CHOICES = ('1A', '2A', '3A', '4A')
+    LEVEL_CHOICES = (
+        ('1A', '1A'),
+        ('2A', '2A'),
+        ('3A', '3A'),
+        ('4A', '4A')
+    )
     beauty_title = models.CharField(max_length=128)
     title = models.CharField(max_length=128)
     other_titles = models.CharField(max_length=128)
     connect = models.CharField(max_length=128)
     add_time = models.DateTimeField(auto_now_add=True)
     coord_id = models.OneToOneField('Coords', on_delete=models.CASCADE)
-    tourist_id = models.ForeignKey(Users, on_delete=models.CASCADE)
+    tourist_id = models.ForeignKey(Tourist, on_delete=models.CASCADE)
     status = models.CharField(max_length=2, choices=STATUS_CHOICES, default=NEW)
     winter_lev = models.CharField(max_length=2, choices=LEVEL_CHOICES, default=a_1)
     spring_lev = models.CharField(max_length=2, choices=LEVEL_CHOICES, default=a_1)
