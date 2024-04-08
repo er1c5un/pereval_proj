6e46548c92ff2e2a4ec48ebd2b66e3b63f45b70f
eric <ericrad@bk.ru>
2024-04-08 21:48:30 +0300

    Доработал модели, добавил миграции

--- pereval/camp/models.py
+++ pereval/camp/models.py
@@ -1,63 +1,73 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
-class Tourist(models.Model):
-    email = models.EmailField(max_length=128, unique=True)
-    last_name = models.CharField(max_length=128)
-    first_name = models.CharField(max_length=128)
-    surname = models.CharField(max_length=128)
-    phone = models.CharField(max_length=128)
+class Tourists(models.Model):
+    """
+    Модель пользователей (туристов)
+    """
+    fam = models.CharField(max_length=100, verbose_name="Фамилия")
+    name = models.CharField(max_length=100, verbose_name="Имя")
+    otc = models.CharField(max_length=100, verbose_name="Отчество")
+    email = models.EmailField(max_length=100, unique=True, verbose_name="Почта")
+    phone = models.CharField(max_length=20, verbose_name="Телефон")
 
 
 class Pereval(models.Model):
-    NEW ='NW'
-    PENDING ='PN'
-    ACCEPTED ='AC'
-    REJECTED ='RJ'
-    STATUS_CHOICES = (
-        ('NW', 'new'),
-        ('PN', 'pending'),
-        ('AC', 'accepted'),
-        ('RJ', 'rejected'),
-    )
-    a_1 = '1A'
-    a_2 = '2A'
-    a_3 = '3A'
-    a_4 = '4A'
-    LEVEL_CHOICES = (
-        ('1A', '1A'),
-        ('2A', '2A'),
-        ('3A', '3A'),
-        ('4A', '4A')
-    )
-    beauty_title = models.CharField(max_length=128)
-    title = models.CharField(max_length=128)
-    other_titles = models.CharField(max_length=128)
-    connect = models.CharField(max_length=128)
-    add_time = models.DateTimeField(auto_now_add=True)
-    coord_id = models.OneToOneField('Coords', on_delete=models.CASCADE)
-    tourist_id = models.ForeignKey(Tourist, on_delete=models.CASCADE)
-    status = models.CharField(max_length=2, choices=STATUS_CHOICES, default=NEW)
-    winter_lev = models.CharField(max_length=2, choices=LEVEL_CHOICES, default=a_1)
-    spring_lev = models.CharField(max_length=2, choices=LEVEL_CHOICES, default=a_1)
-    summer_lev = models.CharField(max_length=2, choices=LEVEL_CHOICES, default=a_1)
-    autumn_lev = models.CharField(max_length=2, choices=LEVEL_CHOICES, default=a_1)
+    """
+    Модель перевала. Хранит в себе основные данные
+    """
+    class Status(models.TextChoices):
+        NEW = "NW", _("Новый")
+        PENDING = "PN", _("В работе")
+        ACCEPTED = "AC", _("Принято")
+        REJECTED = "RJ", _("Не принято")
+
+    beauty_title = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
+    other_titles = models.CharField(max_length=100)
+    connect = models.CharField(max_length=100, default="", verbose_name='Что соединяет')
+    add_time = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    status = models.CharField(max_length=10, choices=Status.choices, default=Status.labels[0], verbose_name='Состояние')
+    tourist_id = models.ForeignKey(to=Tourists, related_name='pereval_user', on_delete=models.CASCADE, verbose_name='Пользователь', null=True)
+    coord_id = models.OneToOneField(to='Coords', related_name='pereval_coord', on_delete=models.CASCADE,
+                                    verbose_name='Координаты')
+    level_id = models.ForeignKey(to='Level', related_name='pereval_level', on_delete=models.CASCADE,
+                                 verbose_name='Сложность', null=True)
 
 
 class Coords(models.Model):
-    latitude = models.DecimalField(decimal_places=8, max_digits=10)
-    longitude = models.DecimalField(decimal_places=8, max_digits=10)
-    height = models.IntegerField(default=0)
+    """
+    Модель для координат
+    """
+    latitude = models.FloatField(verbose_name="Широта")
+    longitude = models.FloatField(verbose_name="Долгота")
+    height = models.IntegerField(verbose_name="Высота")
+
+
+class Level(models.Model):
+    """
+    Модель для уровней сложности перевала
+    """
+    class Levels(models.TextChoices):
+        LEVEL1 = "LV1", _("1А")
+        LEVEL2 = "LV2", _("1Б")
+        LEVEL3 = "LV3", _("2А")
+        LEVEL4 = "LV4", _("2Б")
+        LEVEL5 = "LV5", _("3А")
+        LEVEL6 = "LV6", _("3Б")
+
+    winter = models.CharField(max_length=3, choices=Levels.choices, default=Levels.LEVEL1, verbose_name="Зима")
+    spring = models.CharField(max_length=3, choices=Levels.choices, default=Levels.LEVEL1, verbose_name="Весна")
+    summer = models.CharField(max_length=3, choices=Levels.choices, default=Levels.LEVEL1, verbose_name="Лето")
+    autumn = models.CharField(max_length=3, choices=Levels.choices, default=Levels.LEVEL1, verbose_name="Осень")
 
 
 class Images(models.Model):
-    image = models.ImageField(upload_to='static/images')
-    title = models.CharField(max_length=128)
-    pereval_id = models.ForeignKey(Pereval, on_delete=models.CASCADE, related_name='images')
-
-    def __str__(self):
-        return self.title
-
-
-
-
+    """
+    Модель для фотографий
+    """
+    images = models.ImageField(upload_to='static/images', verbose_name="Фото", default='default_image.jpg')
+    title = models.CharField(max_length=100, verbose_name="Название")
+    pereval_id = models.ForeignKey(to=Pereval, related_name='images', on_delete=models.CASCADE,
+                                   verbose_name='Фотографии', null=True)
